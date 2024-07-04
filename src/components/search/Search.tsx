0da0f7f969f5cfe1372ab04e13a9b745b9f533d7
Volodymyr <authentic.vvv@gmail.com>
2024-07-04 14:59:14 +0300

    ---lollo-

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -14,6 +14,7 @@ export function Search() {
   const {searchQuery, films, status} = useSelector((state: RootState) => state.search)
   
   useEffect(() => {
+    console.log('effect')
     dispatch(fetchSearchFilms(searchQuery))
     setState(searchQuery)
   }, [searchQuery]);

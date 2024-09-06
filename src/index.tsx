a8ba7a0dad4f84c23f8e176dd769d31cfde577b2
Volodymyr <authentic.vvv@gmail.com>
2024-09-06 17:18:02 +0300

    firebase_branch commit

--- src/index.tsx
+++ src/index.tsx
@@ -23,9 +23,7 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_FB_APP_ID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
 
 const router = createBrowserRouter([
   {
@@ -59,7 +57,5 @@ const router = createBrowserRouter([
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-// <React.StrictMode>
   <RouterProvider router={router}/>
-// </React.StrictMode>
 );

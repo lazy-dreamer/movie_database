77d828e549f892189e12754271d1f997607a732e
Volodymyr <authentic.vvv@gmail.com>
2024-09-25 13:00:55 +0300

    debounce fn changed

--- src/services/debounce.ts
+++ src/services/debounce.ts
@@ -1,6 +1,6 @@
 export function debounce<T extends (...args: any[]) => any>(func: T, timeout: number = 300): (...args: Parameters<T>) => void {
   let timer: ReturnType<typeof setTimeout>;
-
+  
   return function (this: void, ...args: Parameters<T>): void {
     clearTimeout(timer);
     timer = setTimeout(() => {

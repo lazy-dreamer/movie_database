d6b6019dc23f9903bb6e654b2ac7b327a968416d
Volodymyr <authentic.vvv@gmail.com>
2024-12-18 16:46:37 +0200

    small bugs fixed

--- src/components/PopularMovies.tsx
+++ src/components/PopularMovies.tsx
@@ -1,72 +1,83 @@
-import React, {useEffect, useRef} from "react";
-import {useDispatch, useSelector} from "react-redux";
-import {Preloader} from "./preloader/Preloader";
-import {ErrorMessage} from "./error/ErrorMessage";
-import {FilmBlock} from "./film_block/FilmBlock";
+import React, { useEffect, useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Preloader } from "./preloader/Preloader";
+import { ErrorMessage } from "./error/ErrorMessage";
+import { FilmBlock } from "./film_block/FilmBlock";
 import fetchPopularFilms from "../redux/thunks/fetchPopularFilms";
-import ReactPaginate from 'react-paginate';
-import {setActivePage} from "../redux/films_slice/filmsSlice";
-import {AppDispatch, RootState} from "../redux/store";
+import ReactPaginate from "react-paginate";
+import { setActivePage } from "../redux/films_slice/filmsSlice";
+import { AppDispatch, RootState } from "../redux/store";
 
 export type TFilm = {
-  "adult": boolean;
-  "backdrop_path": string;
-  "genre_ids": number[];
-  "id": number;
-  "original_language": string;
-  "original_title": string;
-  "overview": string;
-  "popularity": number;
-  "poster_path": string;
-  "release_date": string;
-  "title": string;
-  "video": false;
-  "vote_average": number;
-  "vote_count": number;
-}
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: string;
+  title: string;
+  video: false;
+  vote_average: number;
+  vote_count: number;
+};
 
 function PopularMovies() {
   const dispatch: AppDispatch = useDispatch();
-  const {films, status, errorStatus, totalPages, page} = useSelector((state: RootState) => state.films);
+  const { films, status, errorStatus, totalPages, page } = useSelector(
+    (state: RootState) => state.films,
+  );
   const ref = useRef<HTMLDivElement>(null);
-  
+
   useEffect(() => {
     dispatch(fetchPopularFilms(page));
   }, [page, dispatch]);
-  
+
   useEffect(() => {
     if (films.length === 0) {
       dispatch(fetchPopularFilms(1));
     }
   }, []);
-  
+
   type TSelectedPage = {
-    selected: number
-  }
-  
+    selected: number;
+  };
+
   const handlePageClick = (e: TSelectedPage): void => {
     const selectedPage = e.selected + 1;
     dispatch(setActivePage(selectedPage));
-    
-    window.scrollTo({
-      top: ref.current?.offsetTop,
-      left: 0,
-      behavior: "smooth",
-    });
+    if (ref.current) {
+      const elementOffset =
+        ref.current?.getBoundingClientRect().top + window.scrollY - 90;
+
+      window.scrollTo({
+        top: elementOffset,
+        left: 0,
+        // behavior: "smooth",
+      });
+    }
   };
-  
+
   return (
     <>
-      {status === 'loading' ? <Preloader customClass=''/> : status === 'error' ?
-        <ErrorMessage error={errorStatus}/> :
+      {status === "loading" ? (
+        <Preloader customClass="" />
+      ) : status === "error" ? (
+        <ErrorMessage error={errorStatus} />
+      ) : (
         <>
           <div className="quarter_blocks bottom_offset" ref={ref}>
-            {films?.map((item: TFilm) => <FilmBlock key={item.id} film={item}/>)}
+            {films?.map((item: TFilm) => (
+              <FilmBlock key={item.id} film={item} />
+            ))}
           </div>
-          
+
           {totalPages > 1 && (
             <ReactPaginate
-              className='custom_pagination'
+              className="custom_pagination"
               breakLabel="..."
               nextLabel="Next"
               previousLabel="Previous"
@@ -79,7 +90,7 @@ function PopularMovies() {
             />
           )}
         </>
-      }
+      )}
     </>
   );
 }
--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -1,59 +1,91 @@
-import React, {useEffect, useState} from "react";
-import s from './search.module.scss'
-import {Preloader} from "../preloader/Preloader";
-import {FilmBlock} from "../film_block/FilmBlock";
-import {TFilm} from "../PopularMovies";
-import {AppDispatch, RootState} from "../../redux/store";
-import {useDispatch, useSelector} from "react-redux";
-import {resetSearchQuery, setSearchQuery} from "../../redux/search_slice/searchSlice";
+import React, { useEffect, useState } from "react";
+import s from "./search.module.scss";
+import { Preloader } from "../preloader/Preloader";
+import { FilmBlock } from "../film_block/FilmBlock";
+import { TFilm } from "../PopularMovies";
+import { AppDispatch, RootState } from "../../redux/store";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  resetSearchQuery,
+  setSearchQuery,
+} from "../../redux/search_slice/searchSlice";
 import fetchSearchFilms from "../../redux/thunks/fetchSearchFilms";
 
 export function Search() {
-  const [state, setState] = useState('');
+  const [state, setState] = useState("");
   const dispatch: AppDispatch = useDispatch();
-  const {searchQuery, films, status} = useSelector((state: RootState) => state.search)
-  
+  const { searchQuery, films, status } = useSelector(
+    (state: RootState) => state.search,
+  );
+
   useEffect(() => {
     const throttleTimer = setTimeout(() => {
       dispatch(setSearchQuery(state));
     }, 500);
-    
+
     return () => {
       clearTimeout(throttleTimer);
     };
   }, [state, dispatch]);
-  
+
   useEffect(() => {
-    dispatch(fetchSearchFilms(searchQuery))
-    setState(searchQuery)
+    if (searchQuery.length > 1) {
+      dispatch(fetchSearchFilms(searchQuery));
+      setState(searchQuery);
+    }
   }, [searchQuery, dispatch]);
-  
+
   const onSearchChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
-    setState((e.target as HTMLInputElement).value)
-  }
-  
+    setState((e.target as HTMLInputElement).value);
+  };
+
   const clearSearch = () => {
-    dispatch(resetSearchQuery())
-    setState('')
-  }
-  
-  return <section>
-    <div className="screen_content">
-      <div className={s.search_form_frame}>
-        <h2 className={s.title}>Search movie:</h2>
-        <form action="" className={s.search_form} onSubmit={(e) => e.preventDefault()}>
-          <div className={s.search_field_block}>
-            <input type="text" value={state} placeholder='Search...'
-                   onChange={(e: React.ChangeEvent<HTMLInputElement>) => onSearchChange(e)}/>
-            {searchQuery.length >= 1 &&
-            <button type='button' className={s.clear} onClick={() => clearSearch()}/>}
+    dispatch(resetSearchQuery());
+    setState("");
+  };
+
+  return (
+    <section>
+      <div className="screen_content">
+        <div className={s.search_form_frame}>
+          <h2 className={s.title}>Search movie:</h2>
+          <form
+            action=""
+            className={s.search_form}
+            onSubmit={(e) => e.preventDefault()}
+          >
+            <div className={s.search_field_block}>
+              <input
+                type="text"
+                value={state}
+                placeholder="Search..."
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  onSearchChange(e)
+                }
+              />
+              {searchQuery.length >= 1 && (
+                <button
+                  type="button"
+                  className={s.clear}
+                  onClick={() => clearSearch()}
+                />
+              )}
+            </div>
+          </form>
+        </div>
+        {status === "loading" ? (
+          <Preloader customClass="" />
+        ) : (
+          <div className="quarter_blocks">
+            {films.map((item: TFilm) => (
+              <FilmBlock key={item.id} film={item} />
+            ))}
+            {searchQuery.length > 1 && films.length === 0 && (
+              <h3 className="subtitle">Nothing found :(</h3>
+            )}
           </div>
-        </form>
+        )}
       </div>
-      {status === 'loading' ? <Preloader customClass=""/> : <div className="quarter_blocks">
-        {films.map((item: TFilm) => <FilmBlock key={item.id} film={item}/>)}
-        {searchQuery.length > 1 && films.length === 0 && <h3 className='subtitle'>Nothing found :(</h3>}
-      </div>}
-    </div>
-  </section>
+    </section>
+  );
 }
